dd8f06cb9dae4092cc322068b8e229d8c03ecba6
luosilent <luosilent@163.com>
2018-04-15 12:40:24 +0000

    Finish layout and routes

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -1,9 +1,19 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title>@yield('title', 'Sample') -luosilent</title>
+        <title>
+            @yield('title', 'Sample ') - luosilent
+        </title>
+        <link href="/css/app.css" rel="stylesheet">
+        </link>
     </head>
     <body>
-        @yield('content')
+        @include('layouts._header')
+        <div class="container">
+            <div class="col-md-offset-1 col-md-10">
+                @yield('content')
+        @include('layouts._footer')
+            </div>
+        </div>
     </body>
 </html>
--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -1,4 +1,16 @@
 @extends('layouts.default')
+
 @section('content')
-  <h1>主页</h1>
+  <div class="jumbotron">
+    <h1>Welcome</h1>
+    <p class="lead">
+      你现在所看到的是 <a href="https://laravel-china.org/courses/laravel-essential-training-5.1">娱乐 WebApp</a> 的主页。
+    </p>
+    <p>
+      综合娱乐资讯、你想要的一切尽在这里。
+    </p>
+    <p>
+      <a class="btn btn-lg btn-success" href="{{ route('signup') }}" role="button">现在注册</a>
+    </p>
+  </div>
 @stop
