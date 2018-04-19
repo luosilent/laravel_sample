dfeda60babd91c8dcaecd4836be2ed255b9d35b3
luosilent <luosilent@163.com>
2018-04-19 13:00:57 +0000

    Add account activations

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -4,6 +4,9 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Auth;
+// $usename = Auth::user()->name;
+
+// session()->flash('success', '欢迎回来！'.$usename."大人");
 
 class SessionsController extends Controller
 {
@@ -25,16 +28,19 @@ class SessionsController extends Controller
            'password' => 'required'
        ]);
 
-        if (Auth::attempt($credentials, $request->has('remember'))) {
-		   $usename = Auth::user()->name;
-
-           session()->flash('success', '欢迎回来！'.$usename."大人");
-           return redirect()->intended(route('users.show', [Auth::user()]));
-       } 
-        else {
-           session()->flash('danger', '很抱歉，您的邮箱和密码错误');
+       if (Auth::attempt($credentials, $request->has('remember'))) {
+           if(Auth::user()->activated) {
+               session()->flash('success', '欢迎回来！');
+               return redirect()->intended(route('users.show', [Auth::user()]));
+           } else {
+               Auth::logout();
+               session()->flash('warning', '你的账号未激活，请检查邮箱中的注册邮件进行激活。');
+               return redirect('/');
+           }
+       } else {
+           session()->flash('danger', '很抱歉，您的邮箱和密码不匹配');
            return redirect()->back();
-        }
+       }
     }
 
     public function destroy()
